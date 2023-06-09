a35e5203e12a476faedb5671b8f923ca75ea5979
Seasawher <tellagoodstory@icloud.com>
2023-06-09 16:35:29 +0000

    チャットボットを作る

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -3,24 +3,75 @@ from streamlit_chat import message
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
 from hugchat import hugchat
+from hugchat.login import Login
+from dotenv import load_dotenv
+import os
 
 st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
 
-"""
-# Hello World!
-"""
+load_dotenv()
+
+# login
+sign = Login(os.environ['EMAIL'], os.environ['PASSWORD'])
+cookies = sign.login()
+sign.saveCookies()
+
+# load cookies from usercookies/<email>.json
+# sign = Login(os.environ['EMAIL'], None)
+# cookies = sign.loadCookies() # This will detect if the JSON file exists, return cookies if it does and raise an Exception if it's not.
 
 with st.sidebar:
-    st.title('🤗💬 HugChat')
+    st.title('🤗💬 HugChat App')
     st.markdown('''
     ## About
-    このアプリは LLM と会話できるチャットボットです．
-    下記を使用しています:
-    - [Streamlit](<https://streamlit.io/>)
-    - [HugChat](<https://github.com/Soulter/hugging-chat-api>)
-    - [OpenAssistant/oasst-sft-6-llama-30b-xor](<https://huggingface.co/OpenAssistant/oasst-sft-6-llama-30b-xor>) LLM モデル
+    This app is an LLM-powered chatbot built using:
+    - [Streamlit](https://streamlit.io/)
+    - [HugChat](https://github.com/Soulter/hugging-chat-api)
+    - [OpenAssistant/oasst-sft-6-llama-30b-xor](https://huggingface.co/OpenAssistant/oasst-sft-6-llama-30b-xor) LLM model
 
-    💡: API キーは必要ありません！
+    💡 Note: No API key required!
     ''')
     add_vertical_space(5)
-    st.write('Made with ❤️ by [Data Professor](<https://youtube.com/dataprofessor>)')
+    st.write("Made with ❤️ by [Data Professor](<https://youtube.com/dataprofessor>)")
+
+if "generated" not in st.session_state:
+    st.session_state["generated"] = ["I'm HugChat, How may I help you?"]
+
+if "past" not in st.session_state:
+    st.session_state["past"] = ["Hi!"]
+
+input_container = st.container()
+colored_header(label="", description="", color_name="blue-30")
+response_container = st.container()
+
+
+# User input
+## Function for taking user provided prompt as input
+def get_text():
+    input_text = st.text_input("You: ", "", key="input")
+    return input_text
+
+
+## Applying the user input box
+with input_container:
+    user_input = get_text()
+
+
+# Response output
+## Function for taking user prompt as input followed by producing AI generated responses
+def generate_response(prompt):
+    chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+    response = chatbot.chat(prompt)
+    return response
+
+## Conditional display of AI generated responses as a function of user provided prompts
+with response_container:
+    if user_input:
+        response = generate_response(user_input)
+        st.session_state.past.append(user_input)
+        st.session_state.generated.append(response)
+
+    if st.session_state['generated']:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state['generated'][i], key=str(i))
