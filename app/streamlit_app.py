a2644fc5efd48df6b39a57493eec82fd37170cf7
Seasawher <tellagoodstory@icloud.com>
2023-06-09 15:59:19 +0000

    サイドバーを作成する

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,38 +1,26 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from streamlit_chat import message
+from streamlit_extras.colored_header import colored_header
+from streamlit_extras.add_vertical_space import add_vertical_space
+from hugchat import hugchat
+
+st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+# Hello World!
 """
 
+with st.sidebar:
+    st.title('🤗💬 HugChat')
+    st.markdown('''
+    ## About
+    このアプリは LLM と会話できるチャットボットです．
+    下記を使用しています:
+    - [Streamlit](<https://streamlit.io/>)
+    - [HugChat](<https://github.com/Soulter/hugging-chat-api>)
+    - [OpenAssistant/oasst-sft-6-llama-30b-xor](<https://huggingface.co/OpenAssistant/oasst-sft-6-llama-30b-xor>) LLM モデル
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    💡: API キーは必要ありません！
+    ''')
+    add_vertical_space(5)
+    st.write('Made with ❤️ by [Data Professor](<https://youtube.com/dataprofessor>)')
